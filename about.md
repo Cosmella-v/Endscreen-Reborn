6ac9a6e8f6f6ced2bbc2b1d432cee46e9f1c57a1
Suntle <111027061+Suntle@users.noreply.github.com>
2026-08-04 20:27:25 -0400

    v1.0.0

---
+++ about.md
@@ -0,0 +1,8 @@
+# Endscreen Plus
+
+**This mod brings new life to the end screen!**
+It adds new animations when completing a level.
+
+## Credits
+
+- **Cosmella**: helped me tremendously throughout the modding process
---
+++ src/main.cpp
@@ -0,0 +1,200 @@
+#include <Geode/Geode.hpp>
+using namespace geode::prelude;
+#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/EndLevelLayer.hpp>
+#include <Geode/utils/cocos.hpp>
+using namespace geode::cocos;
+#define getChildFromMainLayer(id) this->m_mainLayer->getChildByID(id)
+#define CCWait(time,target) CCSequence::createWithTwoActions(CCDelayTime::create(time),target)
+
+bool summarySetting = true;
+$on_mod(Loaded) {
+summarySetting = Mod::get()->getSettingValue<bool>("summary-bg-setting");
+    listenForSettingChanges<bool>("summary-bg-setting", [](bool value) {
+		summarySetting = value;
+    });
+};
+
+class $modify(endscreen,EndLevelLayer){
+	void moveCoins(){
+		auto director = CCDirector::get();
+		auto winSize = director->getWinSize();
+		auto coinOneBG = getChildFromMainLayer("coin-1-background");
+		auto coinTwoBG = getChildFromMainLayer("coin-2-background");
+		auto coinThreeBG = getChildFromMainLayer("coin-3-background");
+		//coin y = 0.266
+		// 111 170 230
+		
+		if (coinOneBG){
+			coinOneBG->setPosition(-500,winSize.height*0.266f);
+			coinOneBG->runAction(CCWait(2.8, CCEaseExponentialOut::create(CCMoveTo::create(1.5,{111,winSize.height*0.266f}))));
+			if(auto coinOne = getChildFromMainLayer("coin-1-sprite")){
+				coinOne->setPosition(-500,winSize.height*0.266f);
+				coinOne->runAction(CCWait(2.8, CCEaseExponentialOut::create(CCMoveTo::create(1.5,{111,winSize.height*0.266f}))));
+			}
+		}
+		if (coinTwoBG){
+			coinTwoBG->setPosition(-500,winSize.height*0.266f);
+			coinTwoBG->runAction(CCWait(2.8, CCEaseExponentialOut::create(CCMoveTo::create(1.5,{170,winSize.height*0.266f}))));
+			if(auto coinTwo = getChildFromMainLayer("coin-2-sprite")){
+				coinTwo->setPosition(-500,winSize.height*0.266f);
+				coinTwo->runAction(CCWait(2.8, CCEaseExponentialOut::create(CCMoveTo::create(1.5,{170,winSize.height*0.266f}))));
+			}
+		}
+		if (coinThreeBG){
+			coinThreeBG->setPosition(-500,winSize.height*0.266f);
+			coinThreeBG->runAction(CCWait(2.8, CCEaseExponentialOut::create(CCMoveTo::create(1.5,{230,winSize.height*0.266f}))));
+			if(auto coinThree = getChildFromMainLayer("coin-3-sprite")){
+				coinThree->setPosition(-500,winSize.height*0.266f);
+				coinThree->runAction(CCWait(2.8, CCEaseExponentialOut::create(CCMoveTo::create(1.5,{230,winSize.height*0.266f}))));
+			}
+		}
+	}
+
+	void playEndAnimation(){
+		auto director = CCDirector::get();
+		auto winSize = director->getWinSize();
+		auto summary = getChildFromMainLayer("summary-container");
+		auto summarySize = summary->getContentSize();
+		auto summaryBG = CCScale9Sprite::create("GJ_square01.png");
+		auto hideMenu = this->getChildByID("hide-layer-menu");
+		auto inEditorLevel = false;
+
+
+		
+		if(hideMenu){
+			auto layout = ColumnLayout::create();
+        	layout->setAxisReverse(true);
+        	layout->setAxisAlignment(AxisAlignment::Center);
+
+        	hideMenu->ignoreAnchorPointForPosition(false);
+        	hideMenu->setAnchorPoint({0.5, 1.0});
+        	hideMenu->setContentSize({27, 280});
+        	hideMenu->setPositionY(this->getContentHeight() - 6);
+        	hideMenu->setLayout(layout);
+
+			hideMenu->setPosition({1000,hideMenu->getPositionY()});
+		}
+        
+		/*
+		POSITIONS (X, Y MULT) (top down)
+		level-complete-text: 170, 0.75
+		summary-container: 100, 0.525
+		complete-message: 150, 0.34
+		button-menu: 150, 0.5
+		*/
+		//node->runAction(CCEaseExponentialOut::create(cocos2d::CCMoveTo::create(1,{-500,winSize.height*0.75}),1)); 
+		if (auto node = getChildFromMainLayer("level-complete-text")){
+			node->runAction(CCWait(1.8, CCEaseExponentialOut::create(CCMoveTo::create(1.5,{185,winSize.height*0.77f}))));
+		}
+		if (summary){
+			if (summarySetting){
+				summaryBG->setContentSize({summarySize.width + 20, summarySize.height + 20});
+				summaryBG->setPosition(summary->getPosition());
+				summaryBG->setColor({0,0,0});
+				summaryBG->setOpacity(100);
+				summaryBG->setID("SummaryBG"_spr);
+				summaryBG->setZOrder(-1);
+				m_mainLayer->addChild(summaryBG);
+				summaryBG->runAction(CCWait(2.4, CCEaseExponentialOut::create(CCMoveTo::create(1.3,{175,winSize.height*0.525f}))));
+			}
+			summary->runAction(CCWait(2.4, CCEaseExponentialOut::create(CCMoveTo::create(1.3,{175,winSize.height*0.525f}))));
+		}
+
+		if (auto mhCheatIndicator = this->m_mainLayer->getChildByType<CCLabelBMFont>(0)){
+			mhCheatIndicator->setPosition({-500,winSize.height*0.719f});
+			mhCheatIndicator->runAction(CCWait(2.8, CCEaseExponentialOut::create(CCMoveTo::create(1.3,{40,winSize.height*0.719f}))));
+		}
+		if (auto mhVersion = this->m_mainLayer->getChildByType<CCLabelBMFont>(1)){
+			mhVersion->setPosition({-500,winSize.height*0.688f});
+			mhVersion->runAction(CCWait(2.8, CCEaseExponentialOut::create(CCMoveTo::create(1.3,{60,winSize.height*0.688f}))));
+		}
+
+		if (auto node = getChildFromMainLayer("complete-message")){
+			node->runAction(CCWait(2.8, CCEaseExponentialOut::create(CCMoveTo::create(1.3,{180,winSize.height*0.32f}))));
+		}
+		if (auto node = getChildFromMainLayer("end-text")){
+			node->runAction(CCWait(2.8, CCEaseExponentialOut::create(CCMoveTo::create(1.3,{170,winSize.height*0.32f}))));
+		}
+		if (hideMenu){
+			hideMenu->runAction(CCWait(2.9, CCEaseExponentialOut::create(CCMoveTo::create(1.3,{winSize.width*0.9701f,hideMenu->getPositionY()}))));
+		}
+		moveCoins();
+		if (auto node = getChildFromMainLayer("button-menu")){
+			auto retryButton = node->getChildByID("retry-button");
+			auto exitButton = node->getChildByID("exit-button");
+			auto editButton = node->getChildByID("edit-button");
+			if (!editButton){
+			retryButton->setPosition({-60, retryButton->getPositionY()});
+			exitButton->setPosition({60, exitButton->getPositionY()});
+			}
+			node->runAction(CCWait(3.1, CCEaseExponentialOut::create(CCMoveTo::create(1.3,{175,winSize.height*0.5f}))));
+		}
+	}
+
+
+	virtual void showLayer(bool instant){ // end layer
+		EndLevelLayer::showLayer(true);
+
+		auto director = CCDirector::get();
+		auto winSize = director->getWinSize();
+		if (auto bg = getChildFromMainLayer("background")){
+			bg->setVisible(false);
+		}
+		if (auto cleft = getChildFromMainLayer("chain-left")){
+			cleft->setVisible(false);
+		}
+		if (auto cright = getChildFromMainLayer("chain-right")){
+			cright->setVisible(false);
+		}
+		
+		if (auto node = getChildFromMainLayer("level-complete-text")){
+			node->setPosition({-500,winSize.height*0.75f});
+		}
+		if (auto node = getChildFromMainLayer("summary-container")){
+			node->setPosition({-500,winSize.height*0.525f});
+		}
+		if (auto node = getChildFromMainLayer("complete-message")){ 
+			node->setPosition({-500,winSize.height*0.34f});
+		}
+		if (auto node = getChildFromMainLayer("end-text")){ 
+			node->setPosition({-500,winSize.height*0.34f});
+		}
+		if (auto node = getChildFromMainLayer("button-menu")){
+			node->setPosition({-500,winSize.height*0.5f});
+		}
+		this->setOpacity(0);
+		this->runAction(cocos2d::CCFadeTo::create(1.5, 0x64u));
+		this->playEndAnimation();
+	}
+};
+
+class $modify(play,PlayLayer){
+	struct Fields {
+		bool m_HookEnabled = false;
+	};
+
+	void showCompleteEffect(){
+		m_fields->m_HookEnabled = true;
+		showEndLayer();
+		m_fields->m_HookEnabled = false;
+		PlayLayer::showCompleteEffect();
+	}
+	void showEndLayer(){
+		if (!m_fields->m_HookEnabled){
+			return;
+		}
+		PlayLayer::showEndLayer();
+	}
+	void showCompleteText(){
+		PlayLayer::showCompleteText();
+		auto ggtext = "GJ_practiceComplete_001.png";
+  		if (!m_isPracticeMode){
+    		ggtext = "GJ_levelComplete_001.png";
+		}
+		if (auto gg = getChildBySpriteFrameName(this,ggtext)){
+			gg->setVisible(false);
+		}
+	}
+};
